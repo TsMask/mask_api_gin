d977bb25939f5ebd34e92267ce9e7a777f21a3d0
TsMask <340112800@qq.com>
2023-07-11 15:29:10 +0800

    fix: 路由主页接口

--- src/modules/common/controller/index.go
+++ src/modules/common/controller/index.go
@@ -3,22 +3,22 @@ package controller
 import (
 	"fmt"
 	"mask_api_gin/src/framework/model/result"
+	"mask_api_gin/src/framework/service/ctx"
 
 	"github.com/gin-gonic/gin"
-	"github.com/spf13/viper"
 )
 
 // 路由主页
-var Index = new(index)
+var Index = new(indexController)
 
-type index struct{}
+type indexController struct{}
 
 // 根路由
 //
 // GET /
-func (s *index) Handler(c *gin.Context) {
-	name := viper.GetString("pkg.name")
-	version := viper.GetString("pkg.version")
+func (s *indexController) Handler(c *gin.Context) {
+	name := ctx.Config("pkg.name").(string)
+	version := ctx.Config("pkg.version").(string)
 	str := "欢迎使用%s后台管理框架，当前版本：%s，请通过前端管理地址访问。"
 	c.JSON(200, result.OkMsg(fmt.Sprintf(str, name, version)))
 }
