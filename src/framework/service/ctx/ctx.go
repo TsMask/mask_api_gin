072c9308c9dc9fbdbc32acc5854791ac010a0c07
TsMask <340112800@qq.com>
2023-07-11 15:21:24 +0800

    feat: 上下文服务工具

--- src/framework/service/ctx/ctx.go
+++ src/framework/service/ctx/ctx.go
@@ -1,6 +1,9 @@
 package ctx
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+)
 
 // 查询参数转换MapString
 func QueryMapString(c *gin.Context) map[string]string {
@@ -10,4 +13,17 @@ func QueryMapString(c *gin.Context) map[string]string {
 		queryParams[key] = values[0]
 	}
 	return queryParams
+}
+
+// 获取运行服务环境
+// local prod
+func Env() string {
+	return viper.GetString("env")
+}
+
+// 获取配置信息
+//
+// ctx.Config("pkg.name")
+func Config(key string) interface{} {
+	return viper.Get(key)
 }
