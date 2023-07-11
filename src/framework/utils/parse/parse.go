dfca7151d7b469027c649d2f129322e9bd2d1fe1
TsMask <340112800@qq.com>
2023-07-11 15:22:49 +0800

    feat: 解析工具

--- src/framework/utils/parse/parse.go
+++ src/framework/utils/parse/parse.go
@@ -1,21 +1,53 @@
 package parse
 
-import "strconv"
+import (
+	"image/color"
+	"reflect"
+	"strconv"
+)
 
 // 解析数值型
-func Number(str interface{}) int {
+func Number(str interface{}) int64 {
 	switch str := str.(type) {
 	case string:
 		if str == "" {
 			return 0
 		}
-		if num, err := strconv.Atoi(str); err == nil {
-			return num
+		num, err := strconv.ParseInt(str, 10, 64)
+		if err != nil {
+			return 0
 		}
-	case int:
-		return str
+		return num
+	case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64:
+		return reflect.ValueOf(str).Int()
+	case float32, float64:
+		return int64(reflect.ValueOf(str).Float())
+	default:
+		return 0
 	}
-	return 0
+}
+
+// 解析布尔型
+func Boolean(str interface{}) bool {
+	switch str := str.(type) {
+	case string:
+		if str == "" || str == "false" || str == "0" {
+			return false
+		}
+		// 尝试将字符串解析为数字
+		if num, err := strconv.ParseFloat(str, 64); err == nil {
+			return num != 0
+		}
+		return true
+	case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64:
+		num := reflect.ValueOf(str).Int()
+		return num != 0
+	case float32, float64:
+		num := reflect.ValueOf(str).Float()
+		return num != 0
+	default:
+		return false
+	}
 }
 
 // 数组内字符串去重
@@ -31,4 +63,22 @@ func RemoveDuplicates(ids []string) []string {
 	}
 
 	return uniqueIDSlice
+}
+
+// 解析颜色 #fafafa
+func Color(colorStr string) *color.RGBA {
+	// 去除 # 号
+	colorStr = colorStr[1:]
+
+	// 将颜色字符串拆分为 R、G、B 分量
+	r, _ := strconv.ParseInt(colorStr[0:2], 16, 0)
+	g, _ := strconv.ParseInt(colorStr[2:4], 16, 0)
+	b, _ := strconv.ParseInt(colorStr[4:6], 16, 0)
+
+	return &color.RGBA{
+		R: uint8(r),
+		G: uint8(g),
+		B: uint8(b),
+		A: 255, // 不透明
+	}
 }
