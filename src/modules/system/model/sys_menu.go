d02123ba6b7d5d743cd34382620dd466d734fe71
TsMask <340112800@qq.com>
2023-07-11 15:45:35 +0800

    feat: 菜单服务

---
+++ src/modules/system/model/sys_menu.go
@@ -0,0 +1,46 @@
+package model
+
+// SysMenu 菜单权限对象 sys_menu
+type SysMenu struct {
+	// 菜单ID
+	MenuID string `json:"menuId"`
+	// 菜单名称
+	MenuName string `json:"menuName"`
+	// 父菜单ID 默认0
+	ParentID string `json:"parentId"`
+	// 显示顺序
+	MenuSort int `json:"menuSort"`
+	// 路由地址
+	Path string `json:"path"`
+	// 组件路径
+	Component string `json:"component"`
+	// 是否内部跳转（0否 1是）
+	IsFrame string `json:"isFrame"`
+	// 是否缓存（0不缓存 1缓存）
+	IsCache string `json:"isCache"`
+	// 菜单类型（D目录 M菜单 B按钮）
+	MenuType string `json:"menuType"`
+	// 是否显示（0隐藏 1显示）
+	Visible string `json:"visible"`
+	// 菜单状态（0停用 1正常）
+	Status string `json:"status"`
+	// 权限标识
+	Perms string `json:"perms"`
+	// 菜单图标（#无图标）
+	Icon string `json:"icon"`
+	// 创建者
+	CreateBy string `json:"createBy"`
+	// 创建时间
+	CreateTime int64 `json:"createTime"`
+	// 更新者
+	UpdateBy string `json:"updateBy"`
+	// 更新时间
+	UpdateTime int64 `json:"updateTime"`
+	// 备注
+	Remark string `json:"remark"`
+
+	// ====== 非数据库字段属性 ======
+
+	// 子菜单
+	Children []SysMenu `json:"children,omitempty"`
+}
---
+++ src/modules/system/repository/sys_menu.go
@@ -0,0 +1,45 @@
+package repository
+
+import "mask_api_gin/src/modules/system/model"
+
+// ISysMenu 菜单表 数据层接口
+type ISysMenu interface {
+	// SelectMenuList 查询系统菜单列表
+	SelectMenuList(sysMenu model.SysMenu, userId string) []model.SysMenu
+
+	// SelectMenuPermsByRoleId 根据角色ID查询权限
+	SelectMenuPermsByRoleId(roleId string) []string
+
+	// SelectMenuPermsByUserId 根据用户ID查询权限
+	SelectMenuPermsByUserId(userId string) []string
+
+	// SelectMenuTreeByUserId 根据用户ID查询菜单
+	SelectMenuTreeByUserId(userId string) []model.SysMenu
+
+	// SelectMenuListByRoleId 根据角色ID查询菜单树信息
+	SelectMenuListByRoleId(roleId string, menuCheckStrictly bool) []string
+
+	// SelectMenuById 根据菜单ID查询信息
+	SelectMenuById(menuId string) model.SysMenu
+
+	// HasChildByMenuId 是否存在菜单子节点
+	HasChildByMenuId(menuId string) int
+
+	// CheckMenuExistRole 查询菜单是否存在角色
+	CheckMenuExistRole(menuId string) int
+
+	// InsertMenu 新增菜单信息
+	InsertMenu(sysMenu model.SysMenu) string
+
+	// UpdateMenu 修改菜单信息
+	UpdateMenu(sysMenu model.SysMenu) int
+
+	// DeleteMenuById 删除菜单管理信息
+	DeleteMenuById(menuId string) int
+
+	// CheckUniqueMenuName 校验菜单名称是否唯一
+	CheckUniqueMenuName(menuName, parentId string) string
+
+	// CheckUniqueMenuPath 校验路由地址是否唯一（针对目录和菜单）
+	CheckUniqueMenuPath(path string) string
+}
