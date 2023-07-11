e3ed732a012bc11653bc7740b0daeab8d77542a6
TsMask <340112800@qq.com>
2023-07-11 15:44:54 +0800

    feat: 字典类型服务

---
+++ src/modules/system/model/sys_dict_type.go
@@ -0,0 +1,23 @@
+package model
+
+// SysDictType 字典类型对象 sys_dict_type
+type SysDictType struct {
+	// 字典主键
+	DictID string `json:"dictId"`
+	// 字典名称
+	DictName string `json:"dictName"`
+	// 字典类型
+	DictType string `json:"dictType"`
+	// 状态（0停用 1正常）
+	Status string `json:"status"`
+	// 创建者
+	CreateBy string `json:"createBy"`
+	// 创建时间
+	CreateTime int64 `json:"createTime"`
+	// 更新者
+	UpdateBy string `json:"updateBy"`
+	// 更新时间
+	UpdateTime int64 `json:"updateTime"`
+	// 备注
+	Remark string `json:"remark"`
+}
---
+++ src/modules/system/repository/sys_dict_type.go
@@ -0,0 +1,33 @@
+package repository
+
+import "mask_api_gin/src/modules/system/model"
+
+// ISysDictType 字典类型表 数据层接口
+type ISysDictType interface {
+	// SelectDictTypePage 根据条件分页查询字典类型
+	SelectDictTypePage(query map[string]string) map[string]interface{}
+
+	// SelectDictTypeList 根据条件查询字典类型
+	SelectDictTypeList(sysDictType model.SysDictType) []model.SysDictType
+
+	// SelectDictTypeByID 根据字典类型ID查询信息
+	SelectDictTypeByID(dictID string) model.SysDictType
+
+	// SelectDictTypeByType 根据字典类型查询信息
+	SelectDictTypeByType(dictType string) model.SysDictType
+
+	// CheckUniqueDictName 校验字典名称是否唯一
+	CheckUniqueDictName(dictName string) string
+
+	// CheckUniqueDictType 校验字典类型是否唯一
+	CheckUniqueDictType(dictType string) string
+
+	// InsertDictType 新增字典类型信息
+	InsertDictType(sysDictType model.SysDictType) string
+
+	// UpdateDictType 修改字典类型信息
+	UpdateDictType(sysDictType model.SysDictType) int
+
+	// DeleteDictTypeByID 批量删除字典类型信息
+	DeleteDictTypeByID(dictIDs []string) int
+}
