21869ee47fb6b889338f33fe68296417aad72e4e
TsMask <340112800@qq.com>
2023-07-11 15:49:17 +0800

    feat: 用户与岗位服务

---
+++ src/modules/system/model/sys_user_post.go
@@ -0,0 +1,15 @@
+package model
+
+// SysUserPost 用户和岗位关联对象 sys_user_post
+type SysUserPost struct {
+	UserID string `json:"userId"` // 用户ID
+	PostID string `json:"postId"` // 岗位ID
+}
+
+// NewSysUserPost 创建用户和岗位关联对象的构造函数
+func NewSysUserPost(userID string, postID string) *SysUserPost {
+	return &SysUserPost{
+		UserID: userID,
+		PostID: postID,
+	}
+}
---
+++ src/modules/system/service/sys_user_post.go
@@ -0,0 +1,15 @@
+package service
+
+import "mask_api_gin/src/modules/system/model"
+
+// ISysUserPost 用户与岗位关联表 数据层接口
+type ISysUserPost interface {
+	// CountUserPostByPostId 通过岗位ID查询岗位使用数量
+	CountUserPostByPostId(postId string) int
+
+	// DeleteUserPost 批量删除用户和岗位关联
+	DeleteUserPost(userIds []string) int
+
+	// BatchUserPost 批量新增用户岗位信息
+	BatchUserPost(sysUserPosts []model.SysUserPost) int
+}
