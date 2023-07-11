1b97ef337b754b85d3aa008243ba4d881d09645b
TsMask <340112800@qq.com>
2023-07-11 15:48:37 +0800

    feat: 角色与部门关联服务

---
+++ src/modules/system/model/sys_role_dept.go
@@ -0,0 +1,15 @@
+package model
+
+// SysRoleDept 角色和部门关联对象 sys_role_dept
+type SysRoleDept struct {
+	RoleID string `json:"roleId"` // 角色ID
+	DeptID string `json:"deptId"` // 部门ID
+}
+
+// NewSysRoleDept 创建角色和部门关联对象的构造函数
+func NewSysRoleDept(roleID string, deptID string) *SysRoleDept {
+	return &SysRoleDept{
+		RoleID: roleID,
+		DeptID: deptID,
+	}
+}
---
+++ src/modules/system/repository/sys_role_dept.go
@@ -0,0 +1,12 @@
+package repository
+
+import "mask_api_gin/src/modules/system/model"
+
+// ISysRoleDept 角色与部门关联表 数据层接口
+type ISysRoleDept interface {
+	// DeleteRoleDept 批量删除角色部门关联信息
+	DeleteRoleDept(roleIds []string) int
+
+	// BatchRoleDept 批量新增角色部门信息
+	BatchRoleDept(sysRoleDepts []model.SysRoleDept) int
+}
