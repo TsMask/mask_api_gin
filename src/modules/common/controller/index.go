070ed6a21b7b038071a620d516998e7f43a6c782
TsMask <340112800@qq.com>
2023-07-13 13:54:29 +0800

    fix: 配置参数获取函数变更

--- src/modules/common/controller/index.go
+++ src/modules/common/controller/index.go
@@ -2,8 +2,8 @@ package controller
 
 import (
 	"fmt"
+	"mask_api_gin/src/framework/config"
 	"mask_api_gin/src/framework/model/result"
-	"mask_api_gin/src/framework/service/ctx"
 
 	"github.com/gin-gonic/gin"
 )
@@ -17,8 +17,8 @@ type indexController struct{}
 //
 // GET /
 func (s *indexController) Handler(c *gin.Context) {
-	name := ctx.Config("pkg.name").(string)
-	version := ctx.Config("pkg.version").(string)
+	name := config.Get("pkg.name").(string)
+	version := config.Get("pkg.version").(string)
 	str := "欢迎使用%s后台管理框架，当前版本：%s，请通过前端管理地址访问。"
 	c.JSON(200, result.OkMsg(fmt.Sprintf(str, name, version)))
 }
