dafbd06b62414d99d4f9537fa502f1c43661eaef
TsMask <340112800@qq.com>
2023-07-13 13:53:34 +0800

    feat: jwt令牌生成

--- src/framework/service/token/token.go
+++ src/framework/service/token/token.go
@@ -1,30 +1,66 @@
 package token
 
 import (
+	"encoding/json"
+	"mask_api_gin/src/framework/cache/redis"
+	"mask_api_gin/src/framework/config"
+	"mask_api_gin/src/framework/constants/cachekey"
+	"mask_api_gin/src/framework/constants/token"
+	"mask_api_gin/src/framework/logger"
 	"mask_api_gin/src/framework/model"
-	"mask_api_gin/src/framework/service/permission"
-	systemSystem "mask_api_gin/src/modules/system/model"
+	"mask_api_gin/src/framework/utils/date"
+	"mask_api_gin/src/framework/utils/generate"
+	"time"
+
+	jwt "github.com/golang-jwt/jwt/v5"
 )
 
 // Remove 清除用户登录令牌
 func Remove(token string) bool {
-	_, err := getLoginUser(token)
-	return err == nil
+	return true
 }
 
-// CreateLoginUser 创建登录用户信息对象
-func CreateLoginUser(user systemSystem.SysUser, isAdmin bool) model.LoginUser {
-	loginUser := model.LoginUser{
-		UserID: user.UserID,
-		DeptID: user.DeptID,
-		User:   user,
+// Create 令牌生成
+func Create(loginUser *model.LoginUser, ilobArgs ...string) string {
+	// 生成用户唯一tokne32位
+	loginUser.UUID = generate.Code(32)
+	// 设置请求用户登录客户端
+	loginUser.IPAddr = ilobArgs[0]
+	loginUser.LoginLocation = ilobArgs[1]
+	loginUser.OS = ilobArgs[2]
+	loginUser.Browser = ilobArgs[3]
+	// 设置用户令牌有效期并存入缓存
+	setUserToken(loginUser)
+	// 生成令牌负荷uuid标识
+	jwtToken := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		token.JWT_UUID: loginUser.UUID,
+		token.JWT_KEY:  loginUser.UserID,
+		"exp":          loginUser.ExpireTime,
+		"ait":          loginUser.LoginTime,
+	})
+	// 生成令牌设置密钥
+	key := config.Get("jwt.secret").(string)
+	tokenStr, err := jwtToken.SignedString([]byte(key))
+	if err != nil {
+		logger.Infof("jwt sign err : %v", err)
+		return ""
 	}
-	// 用户权限组标识
-	loginUser.Permissions = permission.Menu(user.UserID, isAdmin)
-	return model.LoginUser{}
+	return tokenStr
 }
 
-// 获取用户身份信息
-func getLoginUser(token string) (model.LoginUser, error) {
-	return model.LoginUser{}, nil
+// setUserToken 设置令牌有效期
+func setUserToken(loginUser *model.LoginUser) {
+	// 计算配置的有效期
+	expTimestamp := config.Get("jwt.expiresIn").(int)
+	expTime := time.Duration(expTimestamp) * time.Minute
+	iatTimestamp := date.NowTimestamp()
+	loginUser.LoginTime = iatTimestamp
+	loginUser.ExpireTime = iatTimestamp + expTime.Milliseconds()
+	// 根据登录标识将loginUser缓存
+	tokenKey := cachekey.LOGIN_TOKEN_KEY + loginUser.UUID
+	jsonBytes, err := json.Marshal(loginUser)
+	if err != nil {
+		return
+	}
+	redis.SetByExpire(tokenKey, string(jsonBytes), expTime)
 }
