b7710f28c779bf45cc953b1c812e8c9a507163f4
TsMask <340112800@qq.com>
2023-07-13 14:48:00 +0800

    fix: 常量包移动

--- src/framework/model/result/result.go
+++ src/framework/model/result/result.go
@@ -1,12 +1,14 @@
 package result
 
-import "mask_api_gin/src/framework/constants"
+import (
+	"mask_api_gin/src/framework/constants/result"
+)
 
 // 响应成功结果 map[string]interface{}{}
 func Ok(v map[string]interface{}) map[string]interface{} {
 	args := make(map[string]interface{})
-	args["code"] = constants.RESULT_CODE_SUCCESS
-	args["msg"] = constants.RESULT_MSG_SUCCESS
+	args["code"] = result.CODE_SUCCESS
+	args["msg"] = result.MSG_SUCCESS
 	// v合并到args
 	for key, value := range v {
 		args[key] = value
@@ -17,7 +19,7 @@ func Ok(v map[string]interface{}) map[string]interface{} {
 // 响应成功结果信息
 func OkMsg(msg string) map[string]interface{} {
 	args := make(map[string]interface{})
-	args["code"] = constants.RESULT_CODE_SUCCESS
+	args["code"] = result.CODE_SUCCESS
 	args["msg"] = msg
 	return args
 }
@@ -25,8 +27,8 @@ func OkMsg(msg string) map[string]interface{} {
 // 响应成功结果数据
 func OkData(data interface{}) map[string]interface{} {
 	args := make(map[string]interface{})
-	args["code"] = constants.RESULT_CODE_SUCCESS
-	args["msg"] = constants.RESULT_MSG_SUCCESS
+	args["code"] = result.CODE_SUCCESS
+	args["msg"] = result.MSG_SUCCESS
 	args["data"] = data
 	return args
 }
@@ -34,8 +36,8 @@ func OkData(data interface{}) map[string]interface{} {
 // 响应失败结果 map[string]interface{}{}
 func Err(v map[string]interface{}) map[string]interface{} {
 	args := make(map[string]interface{})
-	args["code"] = constants.RESULT_CODE_ERROR
-	args["msg"] = constants.RESULT_MSG_ERROR
+	args["code"] = result.CODE_ERROR
+	args["msg"] = result.MSG_ERROR
 	// v合并到args
 	for key, value := range v {
 		args[key] = value
@@ -46,7 +48,7 @@ func Err(v map[string]interface{}) map[string]interface{} {
 // 响应失败结果信息
 func ErrMsg(msg string) map[string]interface{} {
 	args := make(map[string]interface{})
-	args["code"] = constants.RESULT_CODE_ERROR
+	args["code"] = result.CODE_ERROR
 	args["msg"] = msg
 	return args
 }
@@ -54,8 +56,8 @@ func ErrMsg(msg string) map[string]interface{} {
 // 响应失败结果数据
 func ErrData(data interface{}) map[string]interface{} {
 	args := make(map[string]interface{})
-	args["code"] = constants.RESULT_CODE_ERROR
-	args["msg"] = constants.RESULT_MSG_ERROR
+	args["code"] = result.CODE_ERROR
+	args["msg"] = result.MSG_ERROR
 	args["data"] = data
 	return args
 }
