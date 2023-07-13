aa120b5656c2ccc8bed0f847df52e2a82c5c8d52
TsMask <340112800@qq.com>
2023-07-13 13:48:04 +0800

    feat: 令牌常量信息

---
+++ src/framework/constants/token/token.go
@@ -0,0 +1,18 @@
+package token
+
+// 令牌常量信息
+
+// 令牌-数据响应字段
+const RESPONSE_FIELD = "access_token"
+
+// 令牌-请求头标识前缀
+const HEADER_PREFIX = "Bearer "
+
+// 令牌-请求头标识
+const HEADER_KEY = "Authorization"
+
+// 令牌-JWT唯一标识字段
+const JWT_UUID = "login_key"
+
+// 令牌-JWT标识用户主键字段
+const JWT_KEY = "user_id"
