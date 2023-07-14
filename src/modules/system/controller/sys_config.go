0476194a3ca4e490292aa517956d3276d30bdcdc
TsMask <340112800@qq.com>
2023-07-14 13:23:10 +0800

    fix: 包模块修改

--- src/modules/system/controller/sys_config.go
+++ src/modules/system/controller/sys_config.go
@@ -1,11 +1,11 @@
 package controller
 
 import (
-	"mask_api_gin/src/framework/model/result"
-	ctxUtils "mask_api_gin/src/framework/utils/ctx"
-	"mask_api_gin/src/framework/utils/parse"
 	"mask_api_gin/src/modules/system/model"
 	"mask_api_gin/src/modules/system/service"
+	"mask_api_gin/src/pkg/model/result"
+	ctxUtils "mask_api_gin/src/pkg/utils/ctx"
+	"mask_api_gin/src/pkg/utils/parse"
 	"strings"
 
 	"github.com/gin-gonic/gin"
