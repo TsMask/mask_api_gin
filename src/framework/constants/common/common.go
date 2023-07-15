ab503813c851f16537b418ee8bd29548e5a51e50
TsMask <340112800@qq.com>
2023-07-15 10:43:41 +0800

    feat: 去除IP常量

--- src/framework/constants/common/common.go
+++ src/framework/constants/common/common.go
@@ -11,12 +11,6 @@ const HTTP = "http://"
 // https请求
 const HTTPS = "https://"
 
-// IP-网络地址(内网)
-const IP_INNER_ADDR = "::ffff:"
-
-// IP-省市地址(内网)
-const IP_INNER_LOCATION = "内网IP"
-
 // 通用状态标识-正常/成功/是
 const STATUS_YES = "1"
 
