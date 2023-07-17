c79fee7ad3f00b6628274dba6e8d07f1525a00ee
TsMask <340112800@qq.com>
2023-07-17 15:06:00 +0800

    feat: 正则校验用户密码规则

--- src/framework/utils/regular/regular.go
+++ src/framework/utils/regular/regular.go
@@ -4,12 +4,45 @@ import (
 	"mask_api_gin/src/framework/constants/common"
 	"regexp"
 	"strings"
+
+	"github.com/dlclark/regexp2"
 )
 
 // Replace 正则替换
-func Replace(originStr, regStr, repStr string) string {
-	reg := regexp.MustCompile(regStr)
-	return reg.ReplaceAllString(originStr, repStr)
+func Replace(originStr, pattern, repStr string) string {
+	regex := regexp.MustCompile(pattern)
+	return regex.ReplaceAllString(originStr, repStr)
+}
+
+// 判断是否为有效用户名格式
+//
+// 用户名不能以数字开头，可包含大写小写字母，数字，且不少于5位
+func ValidUsername(username string) bool {
+	if username == "" {
+		return false
+	}
+	pattern := `^[a-zA-Z][a-z0-9A-Z]{5,}`
+	match, err := regexp.MatchString(pattern, username)
+	if err != nil {
+		return false
+	}
+	return match
+}
+
+// 判断是否为有效密码格式
+//
+// 密码至少包含大小写字母、数字、特殊符号，且不少于6位
+func ValidPassword(password string) bool {
+	if password == "" {
+		return false
+	}
+	pattern := `^(?![A-Za-z0-9]+$)(?![a-z0-9\W]+$)(?![A-Za-z\W]+$)(?![A-Z0-9\W]+$)[a-zA-Z0-9\W]{6,}$`
+	re := regexp2.MustCompile(pattern, 0)
+	match, err := re.MatchString(password)
+	if err != nil {
+		return false
+	}
+	return match
 }
 
 // 判断是否为http(s)://开头
