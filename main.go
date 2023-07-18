b35dcde7ed1fcd68f52ba25f3ca12481680cdd02
TsMask <340112800@qq.com>
2023-07-18 15:14:46 +0800

    fix: 取消设置cpu核心数

--- main.go
+++ main.go
@@ -1,16 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"mask_api_gin/src"
-	"runtime"
 )
 
 func main() {
-	nuCPU := runtime.NumCPU()
-	runtime.GOMAXPROCS(nuCPU)
-	fmt.Printf("Running with %d CPUs\n", nuCPU)
-
 	src.ConfigurationInit()
 	if err := src.RunServer(); err != nil {
 		src.ConfigurationClose()
