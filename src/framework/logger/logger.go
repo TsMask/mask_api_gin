23f3da9b91617bb29d5875cf7603ea07f069755d
TsMask <340112800@qq.com>
2023-07-20 20:05:48 +0800

    fix: 日志等级设为内部变量

--- src/framework/logger/logger.go
+++ src/framework/logger/logger.go
@@ -8,41 +8,41 @@ import (
 
 const (
 	// Silent silent log level
-	Silent int = iota
+	silent int = iota
 	// Info info log level
-	Info
+	info
 	// Warn warn log level
-	Warn
+	warn
 	// Error error log level
-	Error
+	err
 )
 
-var logMapping = map[int]string{
-	0: "silent",
-	1: "info",
-	2: "warn",
-	3: "error",
+var logLevelMap = map[int]string{
+	silent: "silent",
+	info:   "info",
+	warn:   "warn",
+	err:    "error",
 }
 
 func logWithLevel(level int, format string, v ...interface{}) {
-	if level <= Silent {
+	if level <= silent {
 		return
 	}
 
-	stdLog := log.New(os.Stdout, "["+logMapping[level]+"] ", log.LstdFlags|log.Lshortfile)
+	stdLog := log.New(os.Stdout, "["+logLevelMap[level]+"] ", log.LstdFlags|log.Lshortfile)
 	stdLog.Output(3, fmt.Sprintf(format, v...))
 }
 
 func Infof(format string, v ...interface{}) {
-	logWithLevel(Info, format, v...)
+	logWithLevel(info, format, v...)
 }
 
 func Warnf(format string, v ...interface{}) {
-	logWithLevel(Warn, format, v...)
+	logWithLevel(warn, format, v...)
 }
 
 func Errorf(format string, v ...interface{}) {
-	logWithLevel(Error, format, v...)
+	logWithLevel(err, format, v...)
 }
 
 // Panicf 抛出错误并退出程序
