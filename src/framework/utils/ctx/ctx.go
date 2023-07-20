ac20040606ce1236978416e0af518e28c7f68311
TsMask <340112800@qq.com>
2023-07-20 20:06:29 +0800

    feat: 角色数据范围过滤SQL字符串

--- src/framework/utils/ctx/ctx.go
+++ src/framework/utils/ctx/ctx.go
@@ -2,7 +2,10 @@ package ctx
 
 import (
 	"errors"
+	"fmt"
+	"mask_api_gin/src/framework/config"
 	"mask_api_gin/src/framework/constants/common"
+	"mask_api_gin/src/framework/constants/roledatascope"
 	"mask_api_gin/src/framework/constants/token"
 	"mask_api_gin/src/framework/utils/ip2region"
 	"mask_api_gin/src/framework/utils/ua"
@@ -110,4 +113,80 @@ func LoginUserToUserName(c *gin.Context) string {
 		return loginUser.User.UserName
 	}
 	return ""
+}
+
+// LoginUserToDataScopeSQL 登录用户信息-角色数据范围过滤SQL字符串
+func LoginUserToDataScopeSQL(c *gin.Context, deptAlias string, userAlias string) string {
+	loginUser, err := LoginUser(c)
+	if err != nil {
+		return ""
+	}
+
+	// 登录用户信息
+	userInfo := loginUser.User
+
+	// 如果是管理员，则不过滤数据
+	if config.IsAdmin(userInfo.UserID) {
+		return ""
+	}
+	// 无用户角色
+	if len(userInfo.Roles) <= 0 {
+		return ""
+	}
+
+	// 记录角色权限范围定义添加过, 非自定数据权限不需要重复拼接SQL
+	var scopeKeys []string
+	var conditions []string
+	for _, role := range userInfo.Roles {
+		dataScope := role.DataScope
+
+		if roledatascope.ALL == dataScope {
+			break
+		}
+
+		if roledatascope.CUSTOM != dataScope {
+			hasKey := false
+			for _, key := range scopeKeys {
+				if key == dataScope {
+					hasKey = true
+					break
+				}
+			}
+			if hasKey {
+				continue
+			}
+		}
+
+		if roledatascope.CUSTOM == dataScope {
+			sql := fmt.Sprintf(`%s.dept_id IN ( SELECT dept_id FROM sys_role_dept WHERE role_id = %s )`, deptAlias, role.RoleID)
+			conditions = append(conditions, sql)
+		}
+
+		if roledatascope.DEPT_AND_CHILD == dataScope {
+			sql := fmt.Sprintf(`%s.dept_id IN ( SELECT dept_id FROM sys_dept WHERE dept_id = %s or find_in_set(%s , ancestors ) )`, deptAlias, userInfo.DeptID, userInfo.DeptID)
+			conditions = append(conditions, sql)
+		}
+
+		if roledatascope.SELF == dataScope {
+			// 数据权限为仅本人且没有userAlias别名不查询任何数据
+			if userAlias == "" {
+				sql := fmt.Sprintf(`%s.dept_id = 0`, deptAlias)
+				conditions = append(conditions, sql)
+			} else {
+				sql := fmt.Sprintf(`%s.user_id = %s`, userAlias, userInfo.UserID)
+				conditions = append(conditions, sql)
+			}
+		}
+
+		// 记录角色范围
+		scopeKeys = append(scopeKeys, dataScope)
+	}
+
+	// 构建查询条件语句
+	dataScopeSQL := ""
+	if len(conditions) > 0 {
+		dataScopeSQL = fmt.Sprintf(" and ( %s ) ", strings.Join(conditions, " or "))
+	}
+	fmt.Println("dataScopeSQL======> ", dataScopeSQL)
+	return dataScopeSQL
 }
