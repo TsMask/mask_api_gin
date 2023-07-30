9b86cdcbce1f56a7a90d534d0c7722c2704e6bae
TsMask <340112800@qq.com>
2023-07-30 11:03:26 +0800

    feat: 跨域中间件

--- src/framework/middleware/cors.go
+++ src/framework/middleware/cors.go
@@ -1,22 +1,86 @@
 package middleware
 
 import (
+	"fmt"
+	"mask_api_gin/src/framework/config"
+	"strings"
+
 	"github.com/gin-gonic/gin"
 )
 
+// Cors 跨域
 func Cors() gin.HandlerFunc {
-	return func(context *gin.Context) {
-		method := context.Request.Method
+	return func(c *gin.Context) {
+		// 设置Vary头部
+		c.Header("Vary", "Origin")
+		c.Header("Keep-Alive", "timeout=5")
 
-		context.Header("Access-Control-Allow-Origin", "*")
-		context.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token")
-		context.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PATCH, PUT")
-		context.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
-		context.Header("Access-Control-Allow-Credentials", "true")
-
-		if method == "OPTIONS" {
-			context.AbortWithStatus(204)
+		requestOrigin := c.GetHeader("Origin")
+		if requestOrigin == "" {
+			c.Next()
+			return
 		}
-		context.Next()
+
+		origin := requestOrigin
+		if v := config.Get("cors.origin"); v != nil {
+			origin = v.(string)
+		}
+		c.Header("Access-Control-Allow-Origin", origin)
+
+		if v := config.Get("cors.credentials"); v != nil {
+			if v.(bool) {
+				c.Header("Access-Control-Allow-Credentials", "true")
+			}
+		}
+
+		// OPTIONS
+		if method := c.Request.Method; method == "OPTIONS" {
+			requestMethod := c.GetHeader("Access-Control-Request-Method")
+			if requestMethod == "" {
+				c.Next()
+				return
+			}
+
+			// 响应最大时间值
+			if v := config.Get("cors.maxAge"); v != nil {
+				if v.(int) > 10000 {
+					c.Header("Access-Control-Max-Age", fmt.Sprint(v))
+				}
+			}
+
+			// 允许方法
+			if v := config.Get("cors.allowMethods"); v != nil {
+				var allowMethods = make([]string, 0)
+				for _, s := range v.([]interface{}) {
+					allowMethods = append(allowMethods, s.(string))
+				}
+				c.Header("Access-Control-Allow-Methods", strings.Join(allowMethods, ","))
+			} else {
+				c.Header("Access-Control-Allow-Methods", "GET,HEAD,PUT,POST,DELETE,PATCH")
+			}
+
+			// 允许请求头
+			if v := config.Get("cors.allowHeaders"); v != nil {
+				var allowHeaders = make([]string, 0)
+				for _, s := range v.([]interface{}) {
+					allowHeaders = append(allowHeaders, s.(string))
+				}
+				c.Header("Access-Control-Allow-Headers", strings.Join(allowHeaders, ","))
+			}
+
+			c.AbortWithStatus(204)
+			return
+		}
+
+		// 暴露请求头
+		if v := config.Get("cors.exposeHeaders"); v != nil {
+			var exposeHeaders = make([]string, 0)
+			for _, s := range v.([]interface{}) {
+				exposeHeaders = append(exposeHeaders, s.(string))
+			}
+			c.Header("Access-Control-Expose-Headers", strings.Join(exposeHeaders, ","))
+		}
+
+		c.Next()
 	}
 }
