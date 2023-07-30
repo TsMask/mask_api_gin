620d9fcc61103cd54483c92fb69a83feac2f7d6b
TsMask <340112800@qq.com>
2023-07-30 10:50:37 +0800

    add: referer 配置 referer 的 host 部分

---
+++ src/framework/middleware/security/referer.go
@@ -0,0 +1,73 @@
+package security
+
+import (
+	"mask_api_gin/src/framework/config"
+	"mask_api_gin/src/framework/vo/result"
+	"net/url"
+
+	"github.com/gin-gonic/gin"
+)
+
+// referer 配置 referer 的 host 部分
+func referer(c *gin.Context) {
+	enable := false
+	if v := config.Get("security.csrf.enable"); v != nil {
+		enable = v.(bool)
+	}
+
+	// csrf 校验类型
+	okType := false
+	if v := config.Get("security.csrf.type"); v != nil {
+		vType := v.(string)
+		if vType == "all" || vType == "any" || vType == "referer" {
+			okType = true
+		}
+	}
+	if !okType {
+		return
+	}
+
+	// 忽略请求方法
+	method := c.Request.Method
+	ignoreMethods := []string{"GET", "HEAD", "OPTIONS", "TRACE"}
+	for _, ignore := range ignoreMethods {
+		if ignore == method {
+			return
+		}
+	}
+
+	referer := c.GetHeader("Referer")
+	if referer == "" {
+		c.AbortWithStatusJSON(200, result.ErrMsg("无效 Referer 未知"))
+		return
+	}
+
+	// 获取host
+	u, err := url.Parse(referer)
+	if err != nil {
+		c.AbortWithStatusJSON(200, result.ErrMsg("无效 Referer 未知"))
+		return
+	}
+	host := u.Host
+
+	// 允许的来源白名单
+	refererWhiteList := make([]string, 0)
+	if v := config.Get("security.csrf.refererWhiteList"); v != nil {
+		for _, s := range v.([]interface{}) {
+			refererWhiteList = append(refererWhiteList, s.(string))
+		}
+	}
+
+	if enable && okType {
+		ok := false
+		for _, domain := range refererWhiteList {
+			if domain == host {
+				ok = true
+			}
+		}
+		if !ok {
+			c.AbortWithStatusJSON(200, result.ErrMsg("无效 Referer "+host))
+			return
+		}
+	}
+}
