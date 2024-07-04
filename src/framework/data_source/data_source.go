f4ba670ee5fc51c37f15e9b65472247c1af4a1e3
TsMask <340112800@qq.com>
2024-07-04 22:39:10 +0800

    style: 数据时间类型解析输出改为logger

--- src/framework/data_source/data_source.go
+++ src/framework/data_source/data_source.go
@@ -196,12 +196,12 @@ func SetFieldValue(obj any, fieldName string, value any) {
 			}
 			fieldValue.SetFloat(floatValue)
 		case reflect.Struct:
-			fmt.Printf("%s 时间解析 %s %v \n", fieldName, fieldValue.Type(), value)
+			logger.Infof("%s 时间解析 %s %v \n", fieldName, fieldValue.Type(), value)
 			if fieldValue.Type() == reflect.TypeOf(time.Time{}) && value != nil {
 				// 解析 value 并转换为 time.Time 类型
 				parsedTime, err := time.Parse("2006-01-02 15:04:05 +0800 CST", fmt.Sprintf("%v", value))
 				if err != nil {
-					fmt.Println("时间解析出错:", err)
+					logger.Errorf("时间解析出错: %v", err)
 				} else {
 					// 设置字段的值
 					fieldValue.Set(reflect.ValueOf(parsedTime))
