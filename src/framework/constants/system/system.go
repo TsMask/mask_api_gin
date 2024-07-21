d82e6574265153a046bdcc3098f4958bff537764
TsMask <340112800@qq.com>
2024-07-21 20:03:39 +0800

    fix: 移除通用和管理员常量改为系统常量信息

---
+++ src/framework/constants/system/system.go
@@ -0,0 +1,21 @@
+package system
+
+// 系统常量信息
+
+// RoleId 系统管理员-系统指定角色ID
+const RoleId = "1"
+
+// RoleKey 系统管理员-系统指定角色KEY
+const RoleKey = "system"
+
+// SysPermission 系统管理员-系统指定权限
+const Permission = "*:*:*"
+
+// StatusYes 通用状态标识-正常/成功/是
+const StatusYes = "1"
+
+// StatusNo 通用状态标识-停用/失败/否
+const StatusNo = "0"
+
+// CtxLoginUser 上下文信息-登录用户
+const CtxLoginUser = "login_user"
