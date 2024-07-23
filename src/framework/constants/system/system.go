476ccd98645fcb43baf809e7087ae0049843f6ac
TsMask <340112800@qq.com>
2024-07-23 21:26:52 +0800

    style: Permission 系统管理员-系统指定权限

--- src/framework/constants/system/system.go
+++ src/framework/constants/system/system.go
@@ -8,7 +8,7 @@ const RoleId = "1"
 // RoleKey 系统管理员-系统指定角色KEY
 const RoleKey = "system"
 
-// SysPermission 系统管理员-系统指定权限
+// Permission 系统管理员-系统指定权限
 const Permission = "*:*:*"
 
 // StatusYes 通用状态标识-正常/成功/是
