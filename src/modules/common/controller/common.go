08239ad4d00d837244538da654db399c7dc96504
TsMask <340112800@qq.com>
2024-07-23 21:02:18 +0800

    feat: 通用模块common添加哈希编码功能接口

--- src/modules/common/controller/common.go
+++ src/modules/common/controller/common.go
@@ -1,6 +1,14 @@
 package controller
 
 import (
+	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
+	"encoding/hex"
+	"fmt"
+	"hash"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,9 +20,56 @@ var NewCommon = &CommonController{}
 // PATH /
 type CommonController struct{}
 
-// Hash 哈希加密
+// Hash 哈希编码
 //
-// GET /hash
+// POST /hash
 func (s *CommonController) Hash(c *gin.Context) {
-	c.String(200, "Common Hash")
+	var body struct {
+		Type string `json:"type" binding:"required,oneof=sha1 sha256 sha512 md5"`
+		Str  string `json:"str" binding:"required"`
+	}
+	if err := c.ShouldBindJSON(&body); err != nil {
+		c.JSON(200, gin.H{
+			"code": 400,
+			"msg":  "参数错误",
+		})
+		return
+	}
+
+	var h hash.Hash
+	var err error
+	switch body.Type {
+	case "sha1":
+		h = sha1.New()
+	case "sha256":
+		h = sha256.New()
+	case "sha512":
+		h = sha512.New()
+	case "md5":
+		h = md5.New()
+	default:
+		c.JSON(200, gin.H{
+			"code": 400,
+			"msg":  fmt.Sprintf("不支持的哈希算法: %s", body.Type),
+		})
+		return
+	}
+
+	// 写入需要哈希的数据
+	if _, err = h.Write([]byte(body.Str)); err != nil {
+		c.JSON(500, gin.H{
+			"code": 500,
+			"msg":  "哈希写入错误",
+		})
+		return
+	}
+
+	// 计算哈希值的16进制表示
+	hashed := h.Sum(nil)
+	text := hex.EncodeToString(hashed)
+	c.JSON(200, gin.H{
+		"code": 200,
+		"msg":  "success",
+		"data": text,
+	})
 }
