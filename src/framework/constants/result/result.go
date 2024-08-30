67550d4f8b4609ea098b88f144f0c0deb30052a5
TsMask <340112800@qq.com>
2024-08-30 22:06:55 +0800

    fix: 调整错误码0和成功码1

--- src/framework/constants/result/result.go
+++ src/framework/constants/result/result.go
@@ -4,12 +4,12 @@ package result
 
 const (
 	// CodeError 响应-code错误失败
-	CodeError = 500
+	CodeError = 0
 	// MsgError 响应-msg错误失败
 	MsgError = "失败"
 
 	// CodeSuccess 响应-msg正常成功
-	CodeSuccess = 200
+	CodeSuccess = 1
 	// MsgSuccess 响应-code正常成功
 	MsgSuccess = "成功"
 )
