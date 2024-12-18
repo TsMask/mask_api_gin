08a7d65e8b4faeed6a9c6ae4a9090595ecdc4d5a
TsMask <340112800@qq.com>
2024-12-18 22:07:21 +0800

    fix: db移除map反序列化结构体函数

--- src/framework/database/db/db.go
+++ src/framework/database/db/db.go
@@ -8,10 +8,7 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"reflect"
 	"regexp"
-	"strconv"
-	"strings"
 	"time"
 
 	"gorm.io/driver/mysql"
@@ -120,6 +117,8 @@ func Close() {
 }
 
 // DB 获取数据源
+//
+// source-数据源
 func DB(source string) *gorm.DB {
 	// 不指定时获取默认实例
 	if source == "" {
@@ -128,7 +127,11 @@ func DB(source string) *gorm.DB {
 	return dbMap[source]
 }
 
-// RawDB 原生查询语句
+// RawDB 原生语句查询
+//
+// source-数据源
+// sql-预编译的SQL语句
+// parameters-预编译的SQL语句参数
 func RawDB(source string, sql string, parameters []any) ([]map[string]any, error) {
 	var rows []map[string]any
 	// 数据源
@@ -146,7 +149,11 @@ func RawDB(source string, sql string, parameters []any) ([]map[string]any, error
 	return rows, nil
 }
 
-// ExecDB 原生执行语句
+// ExecDB 原生语句执行
+//
+// source-数据源
+// sql-预编译的SQL语句
+// parameters-预编译的SQL语句参数
 func ExecDB(source string, sql string, parameters []any) (int64, error) {
 	// 数据源
 	db := DB(source)
@@ -164,6 +171,9 @@ func ExecDB(source string, sql string, parameters []any) (int64, error) {
 }
 
 // PageNumSize 分页页码记录数
+//
+// pageNum-页码
+// pageSize-记录数
 func PageNumSize(pageNum, pageSize any) (int, int) {
 	// 记录起始索引
 	num := parse.Number(pageNum)
@@ -177,170 +187,4 @@ func PageNumSize(pageNum, pageSize any) (int, int) {
 		size = 10
 	}
 	return int(num - 1), int(size)
-}
-
-// Unmarshal 将数据从 []map[string]any 反序列化到指定的结构体中
-func Unmarshal(data []map[string]any, v any) error {
-	// 获取目标结构体的反射值
-	val := reflect.ValueOf(v)
-	// 检查 v 是否为指针类型
-	if val.Kind() != reflect.Ptr || val.IsNil() {
-		return fmt.Errorf("v must be a non-nil pointer to a slice of structs")
-	}
-
-	// 获取指向的结构体值
-	elem := val.Elem()
-	// 检查指向的类型是否为切片
-	if elem.Kind() != reflect.Slice {
-		return fmt.Errorf("v must be a pointer to a slice")
-	}
-
-	// 遍历 data 中的每个 map
-	for _, item := range data {
-		// 为每个 map 创建一个新的实例
-		newElem := reflect.New(elem.Type().Elem()).Elem()
-
-		// 遍历结构体的字段
-		for i := 0; i < newElem.NumField(); i++ {
-			field := newElem.Type().Field(i)
-			fieldValue := newElem.Field(i)
-
-			// 获取 GORM column 标签
-			tagStr := field.Tag.Get("gorm")
-			columnName := extractColumnName(tagStr)
-			if columnName == "" {
-				continue
-			}
-
-			// 检查是否有对应的键值
-			if value, ok := item[columnName]; ok {
-				setFieldValue(fieldValue, value)
-			}
-		}
-
-		// 将新的实例添加到目标切片中
-		elem.Set(reflect.Append(elem, newElem))
-	}
-
-	return nil
-}
-
-// extractColumnName 提取以 "column:" 开头的字段名
-func extractColumnName(tag string) string {
-	if !strings.Contains(tag, "column:") {
-		return ""
-	}
-	// 使用分号分割标签
-	parts := strings.Split(tag, ";")
-	for _, part := range parts {
-		// 查找以 "column:" 开头的部分, 提取并返回字段名
-		if strings.HasPrefix(part, "column:") {
-			return strings.TrimPrefix(part, "column:")
-		}
-	}
-	return ""
-}
-
-// setFieldValue 判断结构体内是否存在指定字段并设置值
-func setFieldValue(fieldValue reflect.Value, value any) {
-	// 检查字段是否存在
-	if fieldValue.IsValid() && fieldValue.CanSet() {
-		// 获取字段的类型
-		fieldType := fieldValue.Type()
-
-		// 转换传入的值类型为字段类型
-		switch fieldType.Kind() {
-		case reflect.String:
-			if value == nil {
-				fieldValue.SetString("")
-			} else {
-				fieldValue.SetString(fmt.Sprintf("%v", value))
-			}
-		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-			intValue, err := strconv.ParseInt(fmt.Sprintf("%v", value), 10, 64)
-			if err != nil {
-				intValue = 0
-			}
-			fieldValue.SetInt(intValue)
-		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-			uintValue, err := strconv.ParseUint(fmt.Sprintf("%v", value), 10, 64)
-			if err != nil {
-				uintValue = 0
-			}
-			fieldValue.SetUint(uintValue)
-		case reflect.Float32, reflect.Float64:
-			floatValue, err := strconv.ParseFloat(fmt.Sprintf("%v", value), 64)
-			if err != nil {
-				floatValue = 0
-			}
-			fieldValue.SetFloat(floatValue)
-		case reflect.Struct:
-			logger.Infof("%v 时间解析 %s %v \n", fieldValue, fieldValue.Type(), value)
-			if fieldValue.Type() == reflect.TypeOf(time.Time{}) && value != nil {
-				// 解析 value 并转换为 time.Time 类型
-				parsedTime, err := time.Parse("2006-01-02 15:04:05 +0800 CST", fmt.Sprintf("%v", value))
-				if err != nil {
-					logger.Errorf("时间解析出错: %v", err)
-				} else {
-					// 设置字段的值
-					fieldValue.Set(reflect.ValueOf(parsedTime))
-				}
-			}
-		default:
-			// 设置字段的值
-			fieldValue.Set(reflect.ValueOf(value).Convert(fieldValue.Type()))
-		}
-	}
-}
-
-// ConvertIdsSlice 将 []string 转换为 []any
-func ConvertIdsSlice(ids []int64) []any {
-	// 将 []string 转换为 []any
-	arr := make([]any, len(ids))
-	for i, v := range ids {
-		arr[i] = v
-	}
-	return arr
-}
-
-// KeyPlaceholderByQuery 查询-参数值的占位符
-func KeyPlaceholderByQuery(sum int) string {
-	placeholders := make([]string, sum)
-	for i := 0; i < sum; i++ {
-		placeholders[i] = "?"
-	}
-	return strings.Join(placeholders, ",")
-}
-
-// KeyValuePlaceholderByInsert 插入-键值数据与参数映射键值占位符 keys, values, placeholder
-func KeyValuePlaceholderByInsert(params map[string]any) (string, []any, string) {
-	// 参数映射的键
-	var keys []string
-	// 参数映射的值
-	var values []any
-	// 参数值的占位符
-	var placeholders []string
-
-	for k, v := range params {
-		keys = append(keys, k)
-		values = append(values, v)
-		placeholders = append(placeholders, "?")
-	}
-
-	return strings.Join(keys, ","), values, strings.Join(placeholders, ",")
-}
-
-// KeyValueByUpdate 更新-键值数据 keys, values
-func KeyValueByUpdate(params map[string]any) (string, []any) {
-	// 参数映射的键
-	var keys []string
-	// 参数映射的值
-	var values []any
-
-	for k, v := range params {
-		keys = append(keys, k+"=?")
-		values = append(values, v)
-	}
-
-	return strings.Join(keys, ","), values
 }
